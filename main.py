e71a02e58b924846adfce42aae0da7ced4adf6f2
Fazri Gading <super.fai700@gmail.com>
2023-06-27 04:06:27 +0800

    add copy and move functions, improve codes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class GImageCategorizer:
         self.root.title("Gading's Image Categorizer v1.0")
         self.root.geometry("800x500")
         self.current_image_index = 0
-        self.current_image_filename = ""
+        self.current_image_path = ""
         self.original_file_path = ""
         self.image_list = []
         self.folder_a = "infected"
@@ -18,31 +18,39 @@ class GImageCategorizer:
         self.image_label.pack()
 
         self.select_button = Button(self.root, text="Select Directory", command=self.select_directory)
-        self.folder_a_button = Button(self.root, text="Infected", command=self.copy_to_folder(self.destination_folder_a), state="disabled")
-        self.folder_b_button = Button(self.root, text="Normal", command=self.copy_to_folder(self.destination_folder_b), state="disabled")
+        self.folder_a_button = Button(self.root, text="Infected", state="disabled")
+        self.folder_b_button = Button(self.root, text="Normal", state="disabled")
         self.select_button.pack()
         self.folder_a_button.pack()
         self.folder_b_button.pack()
+
+        self.image_status = Label(self.root, text="0 images left")
+        self.image_status.pack()
+
+    def set_config(self):
+        self.destination_folder_a = os.path.join(self.input_directory, self.folder_a)
+        self.destination_folder_b = os.path.join(self.input_directory, self.folder_b)
+        os.makedirs(self.destination_folder_a, exist_ok=True)
+        os.makedirs(self.destination_folder_b, exist_ok=True)
+        self.folder_a_button.config(state="normal", command=self.copy_to_a)
+        self.folder_b_button.config(state="normal", command=self.copy_to_b)
 
     def select_directory(self):
         self.input_directory = filedialog.askdirectory()
         if self.input_directory:
             self.image_list = os.listdir(self.input_directory)
-            print(len(self.image_list))
-            self.image_list = [x for x in self.image_list if x.endswith(('.JPG', '.jpeg','.png'))]
-            os.makedirs(self.folder_a, exist_ok=True)
-            os.makedirs(self.folder_b, exist_ok=True)
-            self.destination_folder_a = os.path.join(self.input_directory, self.folder_a)
-            self.destination_folder_b = os.path.join(self.input_directory, self.folder_b)
-            self.folder_a_button.config(state="normal")
-            self.folder_b_button.config(state="normal")
+            # print(len(self.image_list))
+            # self.image_list = [x for x in self.image_list if x.endswith(('.JPG', '.jpeg','.png'))]
+            self.image_list = [os.path.join(self.input_directory, filename) for filename in self.image_list if filename.endswith(('.JPG', '.jpeg','.png'))]
+            self.set_config()
             self.display_image()
+            self.images_left = len(self.image_list)
+            self.image_status['text'] = "{} images left".format(self.images_left) # Move feature only (not copy feature)
 
     def display_image(self):
         if self.current_image_index < len(self.image_list):
-            self.current_image_filename = self.image_list[self.current_image_index]
-            self.original_file_path = os.path.join(self.input_directory, self.current_image_filename)
-            image = Image.open(self.original_file_path)
+            self.current_image_path = self.image_list[self.current_image_index]
+            image = Image.open(self.current_image_path)
             image.thumbnail(image.size)
             photo = ImageTk.PhotoImage(image)
             self.image_label.configure(image=photo)
@@ -50,28 +58,38 @@ class GImageCategorizer:
         else:
             messagebox.showinfo("Gading's Image Categorizer v1.0", "No more images.")
 
+    def copy_to_a(self):
+        return self.copy_to_folder(self.destination_folder_a)
+    
+    def copy_to_b(self):
+        return self.copy_to_folder(self.destination_folder_b)
+
     def copy_to_folder(self, destination_folder):
-        destination_path = os.path.join(destination_folder, self.current_image_filename)
+        destination_path = os.path.join(destination_folder, os.path.basename(self.current_image_path))
         shutil.copy2(self.original_file_path, destination_path)
         self.current_image_index += 1
         self.display_image()
 
-    # def move_to_folder_a(self):
-    #     self.move_image_to_folder('infected')
+    def move_to_a(self):
+        return self.move_to_folder(self.destination_folder_a)
 
-    # def move_to_folder_b(self):
-    #     self.move_image_to_folder('normal')
+    def move_to_b(self):
+        return self.move_to_folder(self.destination_folder_b)
 
-    # def move_image_to_folder(self, folder_name):
-    #     image_path = self.image_list[self.current_image_index]
-    #     destination_folder = os.path.join(self.input_directory, folder_name)
-    #     # os.makedirs(destination_folder, exist_ok=True)
-    #     destination_path = os.path.join(destination_folder, image_path)
-    #     os.rename(image_path, destination_path)
-    #     self.current_image_index += 1
-    #     self.display_image()
+    def move_to_folder(self, destination_folder):
+        destination_path = os.path.join(destination_folder, os.path.basename(self.current_image_path))
+        try:
+            os.rename(self.current_image_path, destination_path)
+        except OSError as e:
+            messagebox.showerror("Image Viewer", f"Error moving image: {str(e)}")
+        else:
+            self.current_image_index += 1
+            self.images_left -= 1
+            self.display_image()
+
 
 if __name__ == '__main__':
     root = Tk()
     app = GImageCategorizer(root)
+    root.configure(background="black")
     root.mainloop()
