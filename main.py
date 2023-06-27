d38c58c6b5279b1b7bea1c65ff4e635c8b5b6965
Fazri Gading <super.fai700@gmail.com>
2023-06-27 23:16:27 +0800

    update image preview, window res, add keybind

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ class GImageCategorizer:
     def __init__(self, root):
         self.root = root
         self.root.title("Gading's Image Categorizer v1.0")
-        self.root.geometry("800x500")
+        self.root.geometry("1600x800")
         self.current_image_index = 0
         self.current_image_path = ""
         self.original_file_path = ""
         self.image_list = []
         self.folder_a = "infected"
         self.folder_b = "normal"
+        self.photo = None
         
         self.image_label = Label(self.root)
         self.image_label.pack()
@@ -26,6 +27,9 @@ class GImageCategorizer:
 
         self.image_status = Label(self.root, text="0 images left")
         self.image_status.pack()
+
+        self.root.bind('<Key>', self.handle_key_event)
+        self.root.focus_set()
 
     def set_config(self):
         self.destination_folder_a = os.path.join(self.input_directory, self.folder_a)
@@ -51,10 +55,11 @@ class GImageCategorizer:
         if self.current_image_index < len(self.image_list):
             self.current_image_path = self.image_list[self.current_image_index]
             image = Image.open(self.current_image_path)
-            image.thumbnail(image.size)
-            photo = ImageTk.PhotoImage(image)
-            self.image_label.configure(image=photo)
-            self.image_label.image = photo
+            # image.thumbnail(image.size)
+            image = image.resize((int(image.size[0] * 1.75), int(image.size[1] * 1.75)))
+            self.photo = ImageTk.PhotoImage(image)
+            self.image_label.configure(image=self.photo)
+            # self.image_label.image = photo
         else:
             messagebox.showinfo("Gading's Image Categorizer v1.0", "No more images.")
 
@@ -88,6 +93,11 @@ class GImageCategorizer:
             self.image_status['text'] = "{} images left".format(self.images_left)
             self.display_image()
 
+    def handle_key_event(self, event):
+        if event.keysym == 'o':
+            self.move_to_a()
+        elif event.keysym == 'p':
+            self.move_to_b()
 
 if __name__ == '__main__':
     root = Tk()
